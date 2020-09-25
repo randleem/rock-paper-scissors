cac12587caf7c20c988b123219c6885e77d02963
Emma randle <emmarandle@Emmas-iMac.local>
2020-09-25 10:42:04 +0100

    Task 3 complete

--- rock.js
+++ rock.js
@@ -5,22 +5,25 @@ function getWinner(player1, player2) {
             (player1 === "paper" && player2 === "paper") ||
             (player1 === "scissors" && player2 === "scissors")
         ) {
-            console.log(0);
+            return 0;
         } else if (
             (player1 === "rock" && player2 === "paper") ||
             (player1 === "paper" && player2 === "scissors") ||
             (player1 === "scissors" && player2 === "rock") 
         ) {
-            console.log(-1);
+            return -1;
         } else if (
             (player1 === "rock" && player2 === "scissors") ||
             (player1 === "paper" && player2 === "rock") ||
             (player1 === "scissors" && player2 === "paper")
         ) {
-            console.log(1);
+            return 1;
         }
 }
+
 
 let player1 = prompt("Choose rock paper or scissors");
 
 let result = getWinner(player1, "rock");
+
+alert(result);
