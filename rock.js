40c06e332e21281174708d0066cd10b6dd5f0183
Aftab Hussain <aftabhussainprojects@gmail.com>
2020-09-25 10:37:08 +0100

    Task 2 complete

--- rock.js
+++ rock.js
@@ -5,20 +5,22 @@ function getWinner(player1, player2) {
             (player1 === "paper" && player2 === "paper") ||
             (player1 === "scissors" && player2 === "scissors")
         ) {
-            console.log(0)
+            console.log(0);
         } else if (
             (player1 === "rock" && player2 === "paper") ||
             (player1 === "paper" && player2 === "scissors") ||
             (player1 === "scissors" && player2 === "rock") 
         ) {
-            console.log(-1)
+            console.log(-1);
         } else if (
             (player1 === "rock" && player2 === "scissors") ||
             (player1 === "paper" && player2 === "rock") ||
             (player1 === "scissors" && player2 === "paper")
         ) {
-            console.log(1)
+            console.log(1);
         }
 }
 
-    let result = getWinner("paper", "rock");
+let player1 = prompt("Choose rock paper or scissors");
+
+let result = getWinner(player1, "rock");
